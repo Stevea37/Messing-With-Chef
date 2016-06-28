27a1460031d06b71429afe4b262b5ee4e1f1dddf
Stevea37 <steveay99@gmail.com>
2016-06-28 14:39:54 +0100

    Added template to win-settings

--- cookbooks/win-settings/recipes/default.rb
+++ cookbooks/win-settings/recipes/default.rb
@@ -3,3 +3,10 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+directory 'C:/temp'
+
+template 'C:/temp/server-info.txt' do
+    source 'server-info.txt.erb'
+end
+
